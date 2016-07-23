0fe3a930af219517d4eb60386ba9fb11ad2949c4
SIVASARIKA <sivasarika@sivasarikas-mac.local>
2016-07-23 05:44:12 -0700

    Initial Commit

---
+++ CredicardDetails/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CredicardDetails
+//
+//  Created by SIVASARIKA on 7/23/16.
+//  Copyright © 2016 SIVASARIKA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CredicardDetails/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CredicardDetails
+//
+//  Created by SIVASARIKA on 7/23/16.
+//  Copyright © 2016 SIVASARIKA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
